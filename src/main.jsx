68278314fff261e8a39b852bafcbd0ef6b4b9556
aitbwbooking-ctrl <ai.tbw.booking@gmail.com>
2025-12-11 15:08:59 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-
-// registracija service workera (PWA)
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .catch((err) => console.error("SW registration failed", err));
-  });
-}
